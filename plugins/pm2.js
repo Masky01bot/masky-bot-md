2a82a77e32dfb67cf895cdf59d3bab2c3be2d4ca
Masky01bot <149876684+Masky01bot@users.noreply.github.com>
2024-04-16 15:44:14 +0100

    Add files via upload

---
+++ plugins/pm2.js
@@ -0,0 +1,21 @@
+const { bot } = require('../lib/')
+const { restartInstance } = require('../lib/pm2')
+bot(
+	{
+		pattern: 'reboot ?(.*)',
+		fromMe: true,
+		desc: 'restart with pm2',
+		type: 'misc',
+	},
+	async (message, match) => {
+		await message.send(
+		{
+			text: '⏳',
+			key: message.message.key,
+		},
+		{},
+	    'react' 
+		)
+		restartInstance()
+	}
+)
